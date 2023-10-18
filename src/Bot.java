44e41819ff67fed7063585423746bef5d4f609de
MHEN2606 <matthew.mahendra@gmail.com>
2023-10-18 08:19:16 +0700

    feat: Bot Interface

--- src/Bot.java
+++ src/Bot.java
@@ -1,6 +1,9 @@
-public class Bot {
-    public int[] move() {
-        // create random move
-        return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
-    }
+import javafx.scene.control.Button;
+
+public interface Bot {
+    public int[] move(Button[][] buttons);
+    public int countMark(String mark, Button[][] buttons);
+    public void updateGameBoard(int i , int j, Button[][] buttons);
+    public void setPlayerScore(int i, int j, Button[][] btn);
+    public Button[][] copyState(Button[][] src);
 }
